997f235a3ec7aa7f56436994685024417a51a10a
Alexey Korolev <alphatroya@gmail.com>
2016-07-10 15:47:19 +0300

    Add custom font customizations

--- Example/APMAlertController/ViewController.swift
+++ Example/APMAlertController/ViewController.swift
@@ -273,6 +273,8 @@ class ViewController: UIViewController {
         let alertController = APMAlertController(titleImage: UIImage(named: "alert-controller-error"), message: message, preferredStyle: .Alert)
         alertController.separatorColor = UIColor.whiteColor()
         alertController.buttonTitleColor = UIColor.whiteColor()
+        alertController.customButtonFont = UIFont.systemFontOfSize(30)
+        alertController.customDescriptionFont = UIFont.systemFontOfSize(25)
         alertController.buttonBackgroundColor = UIColor(red: 48 / 255, green: 176 / 255, blue: 214 / 255, alpha: 1)
         let cancelAction = APMAlertAction(title: "Cancel", style: .Cancel) {
             action in
