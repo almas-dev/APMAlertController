84b73e12c01ddc8bce8976ac817ee23d636a3d17
Alexey Korolev <alphatroya@gmail.com>
2017-04-14 12:40:33 +0300

    Add Carthage support

---
+++ Pod/APMAlertController.h
@@ -0,0 +1,19 @@
+//
+//  APMAlertController.h
+//  APMAlertController
+//
+//  Created by Alexey Korolev on 14.04.17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APMAlertController.
+FOUNDATION_EXPORT double APMAlertControllerVersionNumber;
+
+//! Project version string for APMAlertController.
+FOUNDATION_EXPORT const unsigned char APMAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APMAlertController/PublicHeader.h>
+
+
